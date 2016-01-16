dbca2bd14d3c9219fe72cb60384eb2dc5c903c24
Kai <riselikethemoon@gmail.com>
2016-01-16 14:24:44 -0800

    completed io

--- io.js
+++ io.js
@@ -1,6 +1,14 @@
+// import module
 var fs =  require('fs');
+// get filepath
 var file = process.argv[2];
+// get buffer object from file
+var fileBuff = fs.readFileSync(file);
+// convert buffer to string
+var fileContents = fileBuff.toString();
+// split along newlines to count them
+var fileArray = fileContents.split("\n");
 
-var fileContents = fs.readFileSync(file);
+var newlines = fileArray.length - 1;
 
-
+console.log(newlines);
