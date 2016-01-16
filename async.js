5e586964ab16dd11f914cd6151d7b92a68a8dd1c
Kai <riselikethemoon@gmail.com>
2016-01-16 15:21:21 -0800

    tidied, addd solution for reference

--- async.js
+++ async.js
@@ -2,20 +2,25 @@ var fs = require("fs");
 var filePath = process.argv[2];
 var numLines = undefined;
 
-function readFile(callback, path) {
+function readFile(path) {
     // this will take a while
     fs.readFile(path, function doneReading(err, fileBuff){
         // then do this
-        // console.log("called");
         var fileContents = fileBuff.toString();
         var fileArray = fileContents.split("\n");
         var numLines = fileArray.length - 1;
-        callback(numLines);
+        console.log(numLines);
     });
 };
 
-function logNum(numLines) {
-    console.log(numLines);
-}
-
 readFile(logNum, filePath);
+
+// official solution, very tidy!
+// var fs = require('fs')  
+// var file = process.argv[2]  
+
+// fs.readFile(file, function (err, contents) {  
+// // fs.readFile(file, 'utf8', callback) can also be used  
+// var lines = contents.toString().split('\n').length - 1  
+// console.log(lines)  
+// }) 
