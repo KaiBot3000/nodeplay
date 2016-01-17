f137664dff5d3264574b7a879e2e325d7cf3db1b
Kai <riselikethemoon@gmail.com>
2016-01-16 16:33:38 -0800

    started modularizing

---
+++ filtermodule.js
@@ -0,0 +1,50 @@
+// Create a program that prints a list of files in a given directory,  
+// filtered by the extension of the files. You will be provided a directory  
+// name as the first argument to your program (e.g. '/path/to/dir/') and a  
+// file extension to filter by as the second argument.  
+
+// For example, if you get 'txt' as the second argument then you will need to  
+// filter the list to only files that end with .txt. Note that the second  
+// argument will not come prefixed with a '.'.  
+
+// The list of files should be printed to the console, one file per line. You  
+// must use asynchronous I/O.
+
+var fs = require("fs");
+var path = require("path");
+var directory = process.argv[2];
+var filter = process.argv[3];
+
+// function filterFiles(err, fileArray) {
+//     for (i=0; i<fileArray.length; i++) {
+//         if (path.extname(fileArray[i]) === filter) {
+//             console.log(fileArray[i]);
+//         };
+//     };
+// };
+
+// fs.readdir(directory, filterFiles);
+
+// converting to module
+function modularFilter(directory, filter, callback) {
+    fs.readdir(process.argv[2], )
+
+
+}
+
+function callback(filteredFiles) {
+    filteredFiles.forEach(function (file) {
+        console.log(file);
+    });
+};
+
+// official solution
+// var fs = require('fs')  
+// var path = require('path')  
+
+// fs.readdir(process.argv[2], function (err, list) {  
+//   list.forEach(function (file) {  
+//     if (path.extname(file) === '.' + process.argv[3])  
+//       console.log(file)  
+//   })  
+// })
